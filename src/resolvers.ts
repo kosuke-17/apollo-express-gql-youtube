3ed131f73cca17c28f985bef14341abeddd09909
kosuke-17 <tamutamu17@icloud.com>
2022-02-12 12:30:01 +0900

    postデータを取得するモデル作成

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,7 +1,11 @@
+import { Post } from "./models/Post.model";
 export const resolvers = {
   Query: {
     hello: () => {
       return "Hello World";
+    },
+    getAllPosts: async () => {
+      return await Post.find();
     },
   },
 };
