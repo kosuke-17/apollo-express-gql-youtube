cbe278a02ef02953844a208e68f78d104a8751b1
kosuke-17 <tamutamu17@icloud.com>
2022-02-12 11:44:27 +0900

    expressサーバーを適応し、サーバー起動

---
+++ src/server.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => {
+      return "Hello World";
+    },
+  },
+};
+
+async function startServer() {
+  const app = express();
+  const PORT = 4000 || process.env.PORT;
+  // apolloServerをインスタンス化
+  const apoloServer = new ApolloServer({
+    typeDefs: typeDefs,
+    resolvers: resolvers,
+  });
+
+  // apolloServerを起動
+  await apoloServer.start();
+
+  // ミドルウェアとしてexpressサーバーを活用し適応
+  apoloServer.applyMiddleware({ app: app });
+
+  app.use((req, res) => {
+    res.send("Hello from express apollo server");
+  });
+
+  app.listen(PORT, () => console.log(`ポート${PORT}番起動中`));
+}
+startServer();
