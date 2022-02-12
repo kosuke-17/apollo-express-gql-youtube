5b5be37a18438839892f1141b7bb950167086245
kosuke-17 <tamutamu17@icloud.com>
2022-02-12 12:25:16 +0900

    mongoDBに接続する処理

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,16 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs } from "./typeDefs";
 import { resolvers } from "./resolvers";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 async function startServer() {
   const app = express();
   const PORT = 4000 || process.env.PORT;
+  const mongo: string = process.env.MONGO_URI || "";
+
   // apolloServerをインスタンス化
   const apoloServer = new ApolloServer({
     typeDefs: typeDefs,
@@ -21,6 +27,9 @@ async function startServer() {
   app.use((req, res) => {
     res.send("Hello from express apollo server");
   });
+
+  await mongoose.connect(mongo);
+  console.log("MongoDB接続完了");
 
   app.listen(PORT, () => console.log(`ポート${PORT}番起動中`));
 }
